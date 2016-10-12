8b536e86f946c2e70858d1f9844d47966b807d92
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2016-10-12 16:06:03 +0300

    Sämpleriin lisätty kaksi sämpleä ja nappia lisää

--- app/src/main/java/com/testi/peltonpa/sampleri/MainActivity.java
+++ app/src/main/java/com/testi/peltonpa/sampleri/MainActivity.java
@@ -3,6 +3,7 @@ package com.testi.peltonpa.sampleri;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,18 +19,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void playSnare(View view) {
+        try {
+            this.mp.release();
+            this.mp = MediaPlayer.create(this, R.raw.snare);
+        } catch (Exception e) {
+            Log.e("playSnare", "MediaPlayer cannot update data source (audio file may not exist)", e);
+        }
         mp.start();
     }
 
 
     public void playKick(View view) {
+        try {
+            this.mp.release();
+            this.mp = MediaPlayer.create(this, R.raw.kick);
+        } catch (Exception e) {
+            Log.e("playKick", "MediaPlayer cannot update data source (audio file may not exist", e);
+        }
         mp.start();
     }
 
     public void playHat(View view) {
+        try {
+            this.mp.release();
+            this.mp = MediaPlayer.create(this, R.raw.hattu);
+        } catch (Exception e) {
+            Log.e("playKick", "MediaPlayer cannot update data source (audio file may not exist", e);
+        }
         mp.start();
     }
-    
+
 }
