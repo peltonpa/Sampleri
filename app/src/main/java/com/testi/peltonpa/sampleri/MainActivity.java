fb1bb22e7801e8ecc02bcca18a83b44ec39b62d0
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2016-10-12 15:36:08 +0300

    Toimiva luuranko

--- app/src/main/java/com/testi/peltonpa/sampleri/MainActivity.java
+++ app/src/main/java/com/testi/peltonpa/sampleri/MainActivity.java
@@ -1,13 +1,35 @@
 package com.testi.peltonpa.sampleri;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mp = MediaPlayer.create(this, R.raw.snare);
+
     }
+
+
+    public void playSnare(View view) {
+        mp.start();
+    }
+
+
+    public void playKick(View view) {
+        mp.start();
+    }
+
+    public void playHat(View view) {
+        mp.start();
+    }
+    
 }
